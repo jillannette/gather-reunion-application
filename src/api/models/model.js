f7f09bfb2ce9254e83556163d23be07c93cdd729
Jill Arnold <jillarnold2913@gmail.com>
2023-05-08 17:13:31 -0400

    Refactor Member, Reunions

--- src/api/models/model.js
+++ src/api/models/model.js
@@ -19,6 +19,9 @@ const memorySchema = new Schema({
 }, {timestamps: true});
 
 const memberSchema = new Schema({
+  // userName: {type: String, required: true},
+  // password: {type: String, required: true}, //need type validation
+  graduationYear: {type: Number, require: true},
   nameAtGraduation: {type: String, required: true},
   currentName: {type: String, required: true},
   email: {type: String, required: true},
@@ -31,29 +34,32 @@ const memberSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'Comment'
   }],
-  bios: [{type: Schema.Types.ObjectId, 
+  bio: [{type: Schema.Types.ObjectId, 
     ref: 'Bio'
   }]
 }, { timestamps: true });
 
 const bioSchema = new Schema({
   member: {type: Schema.Types.ObjectId, 
-    ref: 'Member'
+  required: true,
+  ref: 'Member'
   },
   image_url: {type: String},
   text: {type: String, required: true}
 }, {timestamps: true});
 
-// const reunionsSchema = new Schema({
-//   year: {type: Number, required: true},
-//   image_url: {type: String},
-//   text: {type: String, required: true}
-// }, {timestamps: true});
+const reunionsSchema = new Schema({
+  nextReunionCounter: {type: Date, required: true},
+  year: {type: Number, required: true},
+  image_url: {type: String},
+  attendees: {type: String},
+  memories: {type: String, required: true}
+}, {timestamps: true});
 
 const Comment = mongoose.model('Comment', commentSchema);
 const Memory = mongoose.model('Memory', memorySchema);
 const Member = mongoose.model('Member', memberSchema);
 const Bio = mongoose.model('Bio', bioSchema);
-//const Reunion = mongoose.model('Reunion', reunionSchema)
+const Reunions = mongoose.model('Reunion', reunionsSchema)
 
-module.exports = {Comment, Memory, Member, Bio};
+module.exports = {Comment, Memory, Member, Bio, Reunions};

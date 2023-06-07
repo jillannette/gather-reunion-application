0455d01bb72bd6294326e9684231b52ca448f547
Jill Arnold <jillarnold2913@gmail.com>
2023-06-07 14:39:18 -0400

    Memories complete

---
+++ src/client/src/components/CreateMemory.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Container, Form, Button } from "react-bootstrap";
+import axios from "axios";
+
+const CreateMemory = ({ loggedInMember }) => {
+  const navigate = useNavigate();
+
+  const [newMemory, setNewMemory] = useState({
+    image_url: "",
+    member: "",
+    subject: "",
+    text: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    console.log("e.target.name", e.target.name);
+    setNewMemory({
+      ...newMemory,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  async function createMemory(e) {
+    setLoading(true);
+    e.preventDefault();
+    console.log(newMemory);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${loggedInMember.token}`,
+      },
+    };
+    axios
+      .post("http://localhost:5000/api/memories", newMemory, config)
+      .then((response) => {
+        console.log(response.data);
+        navigate("/memories");
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+        setError(error);
+      })
+      .finally((loading) => {
+        console.log("Loading data: ", loading);
+        setLoading(false);
+      });
+  }
+
+  if (loading) return "loading...";
+  if (error) return "error";
+
+  return (
+    <>
+      <div className="join-background">
+        <div>
+          <br></br>
+          <h1 className="center-headline">Add A Memory</h1>
+        </div>
+        <Container className="memory-border">
+          <Form className="form" onSubmit={createMemory}>
+            <Form.Group className="mb-3" controlId="formBasicImage">
+              <br></br>
+              <Form.Label>Add a photo of your memory </Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="https://pathtophoto.jpg"
+                name="image_url"
+                value={newMemory.image_url}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicSubject">
+              <Form.Label>Memory Subject</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter memory subject"
+                name="subject"
+                value={newMemory.subject}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicTextArea">
+              <Form.Label>Add your memory here</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                type="text"
+                placeholder="I remember when ..."
+                name="text"
+                value={newMemory.text}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Button variant="warning" type="submit">
+                Submit
+              </Button>
+            </Form.Group>
+          </Form>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default CreateMemory;
--- src/client/src/components/Memories.js
+++ src/client/src/components/Memories.js
@@ -1,69 +1,95 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-  
-const Memories =  () => {
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { Container, Card, Row, Col, Button, ListGroup } from "react-bootstrap";
+import axios from "axios";
 
+const Memories = ({ loggedInMember }) => {
+  const navigate = useNavigate();
 
-  const [memoriesData, setMemoriesData] = useState([]);
-  const token = '{TOKEN}'
+  const [memories, setMemories] = useState([]);
+  const [error, setError] = useState(null);
 
-  axios.get('https://localhost:5000/api/memories', {
-  headers: {
-    'Authorization': `${token}`
+  useEffect(() => {
+    if (loggedInMember) {
+      console.log(loggedInMember);
+      getMemories();
+      console.log(memories);
+    }
+  }, [loggedInMember]);
+
+  async function getMemories() {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${loggedInMember.token}`,
+      },
+    };
+    axios
+      .get("http://localhost:5000/api/memories", config)
+      .then((response) => {
+        console.log("memories", response.data.memories);
+        setMemories(response.data.memories);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+
+        setError(error);
+      })
+      .finally((loading) => {
+        console.log("loading data: ", loading);
+      });
   }
-})
-.then((res) => {
-  setMemoriesData(res.data)
-})
-.catch((error) => {
-  console.error(error)
-})
+
+  if (error) return "error";
 
   return (
+    <div className="member-background">
+      <div className="memories">
+        <br></br>
 
-    <div className="container" style={{borderBottom: "solid 1px", borderWidth: "1px", 
-    paddingBottom: "1rem", textAlign: "center"}}>
+        <div>
+          <div style={{ float: "right" }}>
+            <Link to="/createMemory">
+              <Button variant="light" size="lg" className="add-memory-button">
+                Add a Memory
+              </Button>
+            </Link>
+          </div>
+          <h1 className="center-headline">Memories</h1>
+        </div>
+        <br></br>
+        <br></br>
+      </div>
 
-    <table>
-      <thead>
-        <tr>
-          <th></th>
-          <th>Image</th> 
-          <th>Member</th>
-          <th>Subject</th>
-          <th>Text</th>
-        </tr>
-      </thead>
-
-      <tbody>
-
-        {memoriesData.map(memory => {
-        console.log(memory)
-        return (
-        <tr key={memory.id}>
-          <td><img src={memory.image_url} alt='memory' className="img"></img></td>
-          <td>{memory.member}</td>
-          <td>{memory.subject}</td>
-          <td>{memory.text}</td>
-        </tr>
-
-        )
-        }
-      )}
-      </tbody>
-    </table>
+      <Container>
+        <Row>
+          {memories.map((memory) => {
+            return (
+              <Col key={memory._id} s={12} md={6} lg={4} xl={3}>
+                <Card style={{ width: "15rem" }}>
+                  <Card.Img variant="top" src={memory.image_url} />
+                  <Card.Body className='member-card-body'>
+                  <Card.Text>{memory.subject}</Card.Text>
+                  <Card.Text>{memory.member?.nameAtGraduation + ' remembers: '}</Card.Text>
+                  <ListGroup>
+                  <ListGroup.Item>{memory.text}</ListGroup.Item>
+                  </ListGroup>
+                  </Card.Body>
+                  
+          
+            <Button variant="warning" type="submit">
+              Add comments
+            </Button>
+               
+                </Card>
+                <br></br>
+                <br></br>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
+  );
+};
 
-    );
-}; 
-
-  export default Memories;
-
-//   import { useNavigate } from 'react-router-dom';
-// import Button from 'react-bootstrap/Button';
-
-  // const navigate = useNavigate();
-
-  // <td><Link to={`/viewComments/${comment.id}`}><button>View Comments</button></Link></td>
-
-    
+export default Memories;

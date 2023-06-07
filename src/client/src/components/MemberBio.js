7d76816b7e5a2c10c5d47ac180dacfbb9732564f
Jill Arnold <jillarnold2913@gmail.com>
2023-06-07 14:48:33 -0400

    Members working, need to add edit profile

---
+++ src/client/src/components/MemberBio.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { Card, Container, Row, Col, Button, ListGroup } from 'react-bootstrap';
+import axios from 'axios';
+
+
+const MemberBio = ({loggedInMember}) => {
+
+  const params = useParams();
+  const [memberBio, setMemberBio] = useState({
+    image_url: '',
+    nameAtGraduation: '',
+    currentName: '',
+    email: '',
+    bio: '',
+
+  });
+  
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (loggedInMember) {
+      getMember();
+    }
+  }, [loggedInMember]);
+
+  async function getMember() {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${loggedInMember.token}`,
+      },
+    };
+    await axios
+      .get(`http://localhost:5000/api/members/${params.id}`, config)
+      .then((response) => {
+        console.log("memberBio", response.data);
+        const memberBio = response.data;
+        
+        setMemberBio(memberBio);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+        setError(error);
+      });
+  }
+  
+  if (error) return "error";
+  
+
+  return (
+    <>
+    <Link to='/members'><Button variant="primary" className='back-button' >Back to Members</Button></Link>
+    <Container>
+      <Row>
+        <Col className='member-bio-col'>
+    <Card key={memberBio._id} style={{ width: '40rem'}}>
+ 
+    
+    <Card.Img variant="top" src={memberBio.image_url}/>
+   
+    <Card.Body className='member-card-body'>
+    
+    <Card.Title>{memberBio.currentName}</Card.Title>
+   
+        <Card.Text>({memberBio.nameAtGraduation})</Card.Text>
+        <Card.Text>{memberBio.email}</Card.Text>
+    <Card.Text>{memberBio.bio}</Card.Text>
+    
+    {/* <Button variant="primary" href={member.memories}>Memories</Button>
+    <Button variant="primary" href={member.comments}>Comments</Button> */}
+   
+    
+    </Card.Body>
+    </Card>
+    </Col>
+    </Row>
+    </Container>
+    </>
+  )
+}
+
+export default MemberBio;
--- src/client/src/components/Members.js
+++ src/client/src/components/Members.js
@@ -1,114 +1,107 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import ListGroup from "react-bootstrap/ListGroup";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import axios from "axios";
+
+const Members = ({ loggedInMember }) => {
+  const [members, setMembers] = useState([]);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
 
+  useEffect(() => {
+    if (loggedInMember) {
+      getMembers();
+    } else {
+    }
+  }, [loggedInMember]);
 
-// import fetchData from '../features/apiData';
-
-// const Members members = [
-//     {
-//       "email": "lpatton@gmail.com",
-//       "password": "lpatton4ever",
-//       "nameAtGraduation": "Lorinda Patton",
-//       "currentName": "Lorinda Patton-Oswald",
-//       "image_url": "https://www.designbolts.com/wp-content/uploads/2019/02/beautiful-girl-face-free-stock-photo-1.jpg",
-//       "bio": "I was really shy in high school and I still am.  Look forward to trying to attend a reunion soon"
-//     }
-//   ];
-
-  const MembersPage = () => {
-    return (
-      <>
-    <div className='join-background'>
-    <div className='members'>
-      <br></br>
-      
-    <div>
-    <div style={{float: 'right'}}>
-    <Button variant='light' size='lg' className='add-memory-button'>Edit / Delete My Info</Button>
-    </div>  
-    <h1 className="center-headline">Members</h1>
-    </div>
-
-
-   
-    
-      <br></br>
-      <br></br>
-     
-      </div>
-     
-
-    <Container>
-      <Col>
-      <Row>
-    <Card style={{ width: '18rem' }}>
-     <Card.Img variant="top" src={""}/>
-     <Card.Body>
-     <Card.Title>Name At Graduation</Card.Title>
-     <ListGroup className="list-group-flush">
-         <ListGroup.Item>currentName</ListGroup.Item>
-         <ListGroup.Item>City, State</ListGroup.Item>
-        
-     </ListGroup>
-     <Button variant="warning" href='/bio'>What I've Been Up To</Button>
-    
-    
-     </Card.Body>
-     </Card>
-</Row>
-</Col>
-
-    </Container>
-    </div>
-    </>
-      )
-
-  // }
-
-      
-  // const [members, setMembers] = useState([]);
-  // const membersPerRow = 4;
-
-  // const getMemberData = async () => {
+  async function getMembers() {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${loggedInMember.token}`,
+      },
+    };
+    await axios
+      .get("http://localhost:5000/api/members", config)
+      .then((response) => {
+        console.log("members", response.data);
+        setMembers(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+        setError(error);
+      });
   }
-  //   try {
-  //     await fetchData('/members')
-  //     .getMembers(numOfMembers)
-  //     .then((data) => {
-  //       setMembers(data.json);
-  //     });
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
 
-  // const getColumnsForPage = () => {
-  //   let items = members.map((member) => {
+  if (error) return "error";
 
-  //     return (
-  //       <>
-  //       <h1 className="center-headline">Members</h1>
-  //       <Col>
-       
-  //   </Col>
-  //   </>
-   
-  //   );  
-  //     })
-  //     return items;
-  //   };
+  const getMemberBio = (memberId) => {
+    navigate(`/members/${memberId}`);
+  };
 
-  //   useEffect(() => {
-  //     getMemberData();
-  //   })
+  return (
+    <>
+      <div className="join-background">
+        <div className="members">
+          <br></br>
 
+          <div>
+            <div style={{ float: "right" }}>
+              <Button variant="light" size="lg" className="add-memory-button">
+                Edit / Delete My Info
+              </Button>
+            </div>
+            <h1 className="center-headline">Members</h1>
 
- 
+            <br></br>
+            <br></br>
 
-export default MembersPage;
+            <Container>
+              <Row>
+                {members.map((member) => {
+                  return (
+                    <Col key={member._id} s={12} md={6} lg={4} xl={3}>
+                      <Card style={{ width: "15rem" }}>
+                        <Card.Img
+                          variant="top"
+                          src={`${process.env.PUBLIC_URL}/images/2016.jpg`}
+                        />
+                        <Card.Body className="member-card-body">
+                          <Card.Title>{member.currentName}</Card.Title>
+                          <ListGroup className="list-group-flush">
+                            <ListGroup.Item>
+                              ({member.nameAtGraduation})
+                            </ListGroup.Item>
+                            {/* <ListGroup.Item>City, State</ListGroup.Item> */}
+                          </ListGroup>
+                        </Card.Body>
+                        <Button
+                          onClick={() => getMemberBio(member._id)}
+                          variant="warning"
+                        >
+                          Click here to see what I've been up to
+                        </Button>
+                      </Card>
+                      <br></br>
+                      <br></br>
+                      <br></br>
+                    </Col>
+                  );
+                })}
+              </Row>
+            </Container>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Members;

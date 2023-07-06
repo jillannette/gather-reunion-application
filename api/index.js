e5e4994db57fe3f29e9b272083e2c3cfa8d5e8b7
Jill Arnold <jillarnold2913@gmail.com>
2023-07-06 15:25:35 -0400

    Fix index.js for deployment

--- api/index.js
+++ api/index.js
@@ -11,46 +11,49 @@ const memberRoutes = require("./routes/members.js");
 const memoryRoutes = require("./routes/memories.js");
 const loginRoutes = require("./routes/login.js");
 const reunionRoutes = require("./routes/reunions.js");
-const nextReunionRoutes = require("./routes/nextReunions.js");
+const nextReunionRoutes = require('./routes/nextReunions.js')
 const joinRoutes = require("./routes/join.js");
+const registrationRoutes = require('./routes/registrations.js');
 
 const app = express();
 
-//SUGGESTION FROM STACK OVERFLOW
-// app.use(express.static(path.join(__dirname, "../client/build")))
+// app.use(express.static(path.join(__dirname, "js")));  //DID NOT WORK
+app.use(express.static(path.join(__dirname, "../client/build")))
 
-//
-// const filePath = path.join(__dirname, "public", "index.html");
-// console.log(__dirname);
+const filePath = path.join(__dirname, "public", "index.html");
+console.log(__dirname);
 
-
-// //ENABLE WHEN BACK TO PRODUCTION ENVIRONMENT!
-// app.get("/*", function (req, res) {
-//   res.sendFile("this worked", filePath);
-//   console.log(filePath);
-// });
+app.get("/client", function (req, res) {
+  res.sendFile("this worked", filePath);
+  console.log(filePath);
+});
 
 
 
+// USE THIS??? FOR RENDER DEPLOYMENT????
+// if (process.env.NODE_ENV === 'production') {
+//   //*Set static folder up in production
+//   app.use(express.static('client/build'));
 
-//DO NOT USE IN PRODUCTION MODE
-// app.use((req, res, next) => {
-//   console.log("line 30");
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-//   );
-//   next();
-// });
+//   app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+// }l
+
+app.use((req, res, next) => {
+  console.log('line 30')
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 app.use(express.json());
 
-//FOR DEBUGGING
 // app.use((req, res, next) => {
 //   console.log(req.path, req.method);
 //   next();
@@ -60,15 +63,16 @@ app.use("/api/members", memberAccess, memberRoutes);
 app.use("/api/memories", memberAccess, memoryRoutes);
 app.use("/api/reunions", memberAccess, reunionRoutes);
 app.use("/api/nextReunions", memberAccess, nextReunionRoutes);
+app.use('/api/registrations', memberAccess, registrationRoutes);
 app.use("/api/join", joinRoutes);
 app.use("/api/login", loginRoutes);
 
-//USE WHEN IN PRODUCTION
-if (process.env.MODE === "production") {
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-  });
-}
+
+// if (process.env.MODE==="production") {
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, "../client/build", "index.html"))
+//   })
+// }
 
 app.use(restrictedAccess);
 
